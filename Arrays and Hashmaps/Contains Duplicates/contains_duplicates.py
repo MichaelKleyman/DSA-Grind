dbdfe47b8dacae989444d73ba817f9fe2a506cfe
Michael Kleyman <michaelkleyman99@gmail.com>
2023-07-18 11:53:25 -0400

    finished easy array hashmap leetcode problem

---
+++ Arrays and Hashmaps/Contains Duplicates/contains_duplicates.py
@@ -0,0 +1,16 @@
+
+from typing import List
+
+
+def contains_duplicates(self, nums: List[int]) -> bool:
+    dict = {}
+    for num in nums:
+        if num in dict:
+            return True
+        else:
+            dict[num] = 1
+    return False
+
+arr = [1,2,3,3,4,1,2]
+
+print(contains_duplicates(arr))
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DSA-Grind
+Personal repository to keep track of all the Data Structures and Algorithim problems I do. Working through the Neetcode 150!
