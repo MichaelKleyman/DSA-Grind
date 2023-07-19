c8acbe524d471024a78379e1d4efa42186caebc8
Michael Kleyman <michaelkleyman99@gmail.com>
2023-07-19 16:49:48 -0400

    added 2 more solutions to the valid anagram problem

--- Arrays and Hashmaps/Valid Anagram/valid_anagram.py
+++ Arrays and Hashmaps/Valid Anagram/valid_anagram.py
@@ -2,6 +2,9 @@
 # SOLUTION 1 
 # Time complexity: O(n)
 # Space complexity: O(n)
+from collections import Counter
+
+
 def validAnagram1(s: str, t: str) -> bool:
     if len(s) != len(t):
         return False
@@ -22,10 +25,46 @@ def validAnagram1(s: str, t: str) -> bool:
 s = 'car'
 t = 'acr'
 
-print(validAnagram1(s, t))
+# print(validAnagram1(s, t))
     
 
+# SOLUTION 2 
+# Time complexity: O(n log n)
+# Space complexity: O(1)
 def validAnagram2(s: str, t: str) -> bool:
     return sorted(s) == sorted(t)
 
-print(validAnagram2(s, t))
+# print(validAnagram2(s, t))
+
+
+# SOLUTION 3
+# Time complexity: O(s+t) --- O(n)
+# Space complexity: O(1)
+def validAnagram3(s: str, t: str) -> bool:
+    return Counter(s) == Counter(t)
+
+# print(validAnagram3(s, t))
+
+# SOLUTION 4
+# Time complexity: 
+    # sorting takes O(n log n)
+    # and the loop is O(n) because of one for loop
+# Space complexity: O(n)
+def validAnagram4(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    
+    s_list, t_list = list(s), list(t)
+
+    s_list.sort()
+    t_list.sort()
+
+    for i, n in enumerate(s_list):
+        if n != t_list[i]:
+            return False
+    return True
+
+
+print(validAnagram4(s, t))
+
+
