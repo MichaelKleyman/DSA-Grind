139c34e064b6fd557213d4f72d7ee21372931d7e
Michael Kleyman <michaelkleyman99@gmail.com>
2023-08-31 13:53:29 -0400

    added space and time complexity for the reverse linked list solution

--- Linked Lists/Reverse Linked List/reverse_linked_list.py
+++ Linked Lists/Reverse Linked List/reverse_linked_list.py
@@ -7,6 +7,8 @@ class ListNode:
         self.val = val
         self.next = next
 
+# TIME COMPLEXITY: 0(n) Linear time complexity because we're iterating through the list only once
+# SPACE COMPLEXITY: 0(1) Constant time complexity because the variable size doesnt grow with the input size
 
 def reverseLinkedList(head: Optional[ListNode]) -> Optional[ListNode]:
 
