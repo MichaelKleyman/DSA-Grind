7d1b69f2bee3247414daab99715166d77e9e63d3
Michael Kleyman <michaelkleyman99@gmail.com>
2023-08-31 11:21:22 -0400

    added second alternative solution to valid palindrome problem

--- Two Pointers/Valid Palindrome/valid_palindrome.py
+++ Two Pointers/Valid Palindrome/valid_palindrome.py
@@ -26,4 +26,35 @@ def isPalindrome(s: str) -> bool:
 
 s = "A man, a plan, a canal: Panama"
 
-print(isPalindrome(s))
+# print(isPalindrome(s))
+
+
+# TIME COMPLEXITY: O(n) Linear time complexity : due to two loops happening, but not inner loops
+# SPACE COMPLEXITY: 0(1) Constant time complexity: due no memory being taken up by not making any new strings
+
+def isPalindrome2(s: str) -> bool:
+    if len(s) == 0:
+        return True
+
+    l, r = 0, len(s) - 1
+
+    while l <= r:
+
+        while l < r and not isAlphaNum(s[l]):
+            l += 1
+
+        while r > l and not isAlphaNum(s[r]):
+            r -= 1
+
+        if s[l].lower() != s[r].lower():
+            return False
+
+        l, r = l + 1, r - 1
+    
+    return True
+
+
+def isAlphaNum(c) -> bool:
+    return ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9')
+
+print(isPalindrome2(s))
